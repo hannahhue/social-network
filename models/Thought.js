b416d7d7ae19c49c2ad3dd71f98e087e5bc29bdd
unknown <alicia9790972@gmail.com>
2022-11-09 21:20:09 -0500

    inital commit + models bc i forgot to commit before ):

---
+++ models/Thought.js
@@ -0,0 +1,42 @@
+//text
+//string
+//req
+//char limit
+
+const { Schema, model } = require("mongoose");
+
+const thoughtSchema = new Schema(
+  {
+    thoughtText: {
+      type: String,
+      required: true,
+      max_length: 280,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now(),
+      get: (date) => {
+        if (date) return date.toISOString().split("T")[0];
+      },
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    reactions: [reactionSchema],
+  },
+  {
+    toJSON: {
+      getters: true,
+    },
+    id: false,
+  }
+);
+
+const Thought = model("thought", thoughtSchema);
+
+userSchema.virtual("reactionCount").get(function () {
+  return this.reaction.length;
+});
+
+module.exports = Thought;
